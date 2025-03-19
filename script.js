6e5a7f9a9a3befaeca050f5d5d3ee67f053a76fd
xlcrscience <sciencehod@excelsior.edu.np>
2025-03-19 10:17:16 +0545

    Update script.js

--- script.js
+++ script.js
@@ -186,10 +186,10 @@ function appendMessage(sender, message, isHtml = false) {
 async function typeMessage(element, text) {
     element.classList.add('typing');
     element.innerHTML = '';
-    await new Promise(resolve => setTimeout(resolve, 500));
+    await new Promise(resolve => setTimeout(resolve, 90));
     for (let i = 0; i < text.length; i++) {
         element.innerHTML = text.substring(0, i + 1);
-        await new Promise(resolve => setTimeout(resolve, 20));
+        await new Promise(resolve => setTimeout(resolve, 1));
     }
     element.classList.remove('typing');
 }
